7846c6e9ae93ca7864ae85516362f407f8893319
Ito Yuki <lapislazuli2930@gmail.com>
2025-09-28 21:33:40 +0900

    ログインルートエラー改善

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/deployRepository/",
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+  },
 });

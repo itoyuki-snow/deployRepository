1b9ed27522b15995edf5448b26883054a55b7a4a
Ito Yuki <lapislazuli2930@gmail.com>
2025-09-30 09:15:00 +0900

    VITEエラー改善

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
   base: "/deployRepository/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
-  },
+  plugins: [react()],
 });
